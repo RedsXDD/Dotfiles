89726d9ce622ff5fd3aac116c05b18394a6d13cd
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-04-17 18:02:17 -0300

    Improved surround keymappings.

--- .config/nvim/lua/plugins/mini/surround.lua
+++ .config/nvim/lua/plugins/mini/surround.lua
@@ -1,5 +1,23 @@
 return {
 	"echasnovski/mini.surround",
-	keys = { "sa", "sd", "sr", "sf", "sF", "sh", "sn" },
-	opts = {},
+	keys = {
+		{ "<Leader>sa", mode = { "n", "v" }, desc = "Add Surrounding." },
+		{ "<Leader>sd", desc = "Delete Surrounding." },
+		{ "<Leader>sr", desc = "Replace Surrounding." },
+		{ "<Leader>sf", desc = "Find Right Surrounding." },
+		{ "<Leader>sF", desc = "Find Left Surrounding." },
+		{ "<Leader>sh", desc = "Highlight Surrounding." },
+		{ "<Leader>sn", desc = "Update `MiniSurround.config.n_lines`." },
+	},
+	opts = {
+		mappings = {
+			add = "<Leader>sa", -- Add surrounding in Normal and Visual modes.
+			delete = "<Leader>sd", -- Delete surrounding.
+			find = "<Leader>sf", -- Find surrounding (to the right).
+			find_left = "<Leader>sF", -- Find surrounding (to the left).
+			highlight = "<Leader>sh", -- Highlight surrounding.
+			replace = "<Leader>sr", -- Replace surrounding.
+			update_n_lines = "<Leader>sn", -- Update `n_lines`.
+		},
+	},
 }
