f4761f55cf39a5f2d539286f38b428e3a4ed316d
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-04-18 21:56:40 -0300

    Changed single quote to double quote.

--- .config/nvim/lua/pywal/highlights.lua
+++ .config/nvim/lua/pywal/highlights.lua
@@ -1,5 +1,5 @@
 local M = {}
-local config = require('pywal.config')
+local config = require("pywal.config")
 
 function M.highlight_all(colors)
 	local base_highlights = config.highlights_base(colors)
