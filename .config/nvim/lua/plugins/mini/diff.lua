53ab48895e88d74614dfc494f01a15bd86c1ae59
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-04-30 22:48:09 -0300

    Changed main keybinding from 'gh' to '<Leader>d'

--- .config/nvim/lua/plugins/mini/diff.lua
+++ .config/nvim/lua/plugins/mini/diff.lua
@@ -2,34 +2,35 @@ return {
 	"echasnovski/mini.diff",
 	event = { "BufReadPost", "BufNewFile" },
 	keys = {
-		{ "gh", "gH", "[h", "]h", "[H", "]H" },
-		{ "<leader>go", function() require("mini.diff").toggle_overlay(0) end, desc = "Toggle mini.diff overlay." },
+		{ "[h", "]h", "[H", "]H" },
+		{ "<Leader>d" },
+		{ "<Leader>do", function() require("mini.diff").toggle_overlay(0) end, desc = "Toggle mini.diff overlay." },
 	},
 	opts = {
-		delay = { text_change = 200, }, -- How much to wait before update following every text change.
+		delay = { text_change = 200 }, -- How much to wait before update following every text change.
 		view = {
 			-- Default: 'number' if line numbers are enabled, 'sign' otherwise.
-			style = vim.go.number and 'number' or 'sign',
+			style = vim.go.number and "number" or "sign",
 			signs = {
-				add     = require("user.icons").icons.gitsigns.add,
-				change  = require("user.icons").icons.gitsigns.change,
-				delete  = require("user.icons").icons.gitsigns.delete,
+				add = require("user.icons").icons.gitsigns.add,
+				change = require("user.icons").icons.gitsigns.change,
+				delete = require("user.icons").icons.gitsigns.delete,
 			},
 			priority = vim.highlight.priorities.user - 1, -- Priority of used visualization extmarks.
 		},
 		mappings = {
-			apply = 'gh', -- Apply hunks inside a visual/operator region.
-			reset = 'gH', -- Reset hunks inside a visual/operator region.
-			textobject = 'gh', -- Hunk range textobject to be used inside operator.
+			textobject = "<Leader>dh", -- Hunk range textobject to be used inside operator.
+			apply = "<Leader>dh", -- Apply hunks inside a visual/operator region.
+			reset = "<Leader>dH", -- Reset hunks inside a visual/operator region.
 
 			-- Go to hunk range in corresponding direction.
-			goto_first = '[H',
-			goto_prev = '[h',
-			goto_next = ']h',
-			goto_last = ']H',
+			goto_first = "[H",
+			goto_prev = "[h",
+			goto_next = "]h",
+			goto_last = "]H",
 		},
 		options = {
-			algorithm = 'histogram',
+			algorithm = "histogram",
 			indent_heuristic = true,
 			linematch = 60,
 			wrap_goto = false,
