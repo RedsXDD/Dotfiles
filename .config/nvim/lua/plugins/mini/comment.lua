3282ec2d30429c0b2f1f80a9750b30c2750d609d
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-04-30 22:48:32 -0300

    Changed main keybinding from 'gc' to '<Leader>c'

--- .config/nvim/lua/plugins/mini/comment.lua
+++ .config/nvim/lua/plugins/mini/comment.lua
@@ -5,9 +5,15 @@ return {
 		{ "JoosepAlviste/nvim-ts-context-commentstring", opts = { enable_autocmd = false } },
 	},
 	keys = {
-		{ "gc", mode = { "n", "x" }, desc = "Comment" },
+		{ "<Leader>c", mode = { "n", "x" }, desc = "+Comment" },
 	},
 	opts = {
+		mappings = {
+			comment = '<Leader>c',        -- Toggle comment on Normal and Visual modes.
+			comment_visual = '<Leader>c', -- Toggle comment on visual selection.
+			comment_line = '<Leader>cc',  -- Toggle comment on current line.
+			textobject = '<Leader>c', -- Define 'comment' textobject (like `d<Leader>c` - delete whole comment block). Works also in Visual mode if mapping differs from `comment_visual`.
+		},
 		options = {
 			ignore_blank_line = true,
 			custom_commentstring = function()
