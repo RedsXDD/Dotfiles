b3a937d13671422483ad1ebcaab632d83bd4f268
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-02 19:41:04 -0300

    Changed some comments structures & Formatted file with stylua

--- .config/nvim/lua/plugins/mini/starter.lua
+++ .config/nvim/lua/plugins/mini/starter.lua
@@ -15,36 +15,41 @@ return {
 		local header = [[]]
 		if vim.env.DISPLAY ~= nil then
 			header = table.concat({
-				[[                                                                     ]],
-				[[       ████ ██████           █████      ██                     ]],
-				[[      ███████████             █████                             ]],
-				[[      █████████ ███████████████████ ███   ███████████   ]],
-				[[     █████████  ███    █████████████ █████ ██████████████   ]],
-				[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-				[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-				[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+				[[                                                                   ]],
+				[[      ████ ██████           █████      ██                    ]],
+				[[     ███████████             █████                            ]],
+				[[     █████████ ███████████████████ ███   ███████████  ]],
+				[[    █████████  ███    █████████████ █████ ██████████████  ]],
+				[[   █████████ ██████████ █████████ █████ █████ ████ █████  ]],
+				[[ ███████████ ███    ███ █████████ █████ █████ ████ █████ ]],
+				[[██████  █████████████████████ ████ █████ █████ ████ ██████]],
 			}, "\n")
 		else
 			header = table.concat({
-				[[      ___           ___           ___           ___                       ___      ]],
-				[[     /\__\         /\  \         /\  \         /\__\          ___        /\__\     ]],
-				[[    /::|  |       /::\  \       /::\  \       /:/  /         /\  \      /::|  |    ]],
-				[[   /:|:|  |      /:/\:\  \     /:/\:\  \     /:/  /          \:\  \    /:|:|  |    ]],
-				[[  /:/|:|  |__   /::\~\:\  \   /:/  \:\  \   /:/__/  ___      /::\__\  /:/|:|__|__  ]],
-				[[ /:/ |:| /\__\ /:/\:\ \:\__\ /:/__/ \:\__\  |:|  | /\__\  __/:/\/__/ /:/ |::::\__\ ]],
-				[[ \/__|:|/:/  / \:\~\:\ \/__/ \:\  \ /:/  /  |:|  |/:/  / /\/:/  /    \/__/~~/:/  / ]],
-				[[     |:/:/  /   \:\ \:\__\    \:\  /:/  /   |:|__/:/  /  \::/__/           /:/  /  ]],
-				[[     |::/  /     \:\ \/__/     \:\/:/  /     \::::/__/    \:\__\          /:/  /   ]],
-				[[     /:/  /       \:\__\        \::/  /       ~~~~         \/__/         /:/  /    ]],
-				[[     \/__/         \/__/         \/__/                                   \/__/     ]],
+				[[     ___           ___           ___           ___                       ___     ]],
+				[[    /\__\         /\  \         /\  \         /\__\          ___        /\__\    ]],
+				[[   /::|  |       /::\  \       /::\  \       /:/  /         /\  \      /::|  |   ]],
+				[[  /:|:|  |      /:/\:\  \     /:/\:\  \     /:/  /          \:\  \    /:|:|  |   ]],
+				[[ /:/|:|  |__   /::\~\:\  \   /:/  \:\  \   /:/__/  ___      /::\__\  /:/|:|__|__ ]],
+				[[/:/ |:| /\__\ /:/\:\ \:\__\ /:/__/ \:\__\  |:|  | /\__\  __/:/\/__/ /:/ |::::\__\]],
+				[[\/__|:|/:/  / \:\~\:\ \/__/ \:\  \ /:/  /  |:|  |/:/  / /\/:/  /    \/__/~~/:/  /]],
+				[[    |:/:/  /   \:\ \:\__\    \:\  /:/  /   |:|__/:/  /  \::/__/           /:/  / ]],
+				[[    |::/  /     \:\ \/__/     \:\/:/  /     \::::/__/    \:\__\          /:/  /  ]],
+				[[    /:/  /       \:\__\        \::/  /       ~~~~         \/__/         /:/  /   ]],
+				[[    \/__/         \/__/         \/__/                                   \/__/    ]],
 			}, "\n")
 		end
 		header = table.concat({ header, "\n" .. padding .. [[TIP: To exit Neovim, just run $sudo rm -rf /*]] }, "\n")
 
 		local footer = (function()
-			-- NOTE: This timer is needed because, without it, the time delay displayed for the loading of neovim plugins is always 0ms,
-			-- as the call of the require("mini.starter").refresh() function is needed in order for the right delay to be displayed on the footer.
-			-- NOTE: The call to the require("mini.starter").refresh() function is only needed when opening neovim on a terminal.
+			--[[
+				NOTE:
+					This timer is needed because, without it, the time delay displayed for the loading of neovim plugins is always 0ms,
+					as the call of the require("mini.starter").refresh() function is needed in order for the right delay to be displayed on the footer.
+				NOTE:
+					The call to the require("mini.starter").refresh() function is only needed when opening neovim on a terminal.
+			]]
+			-- stylua: ignore start
 			local delay = 1
 			vim.wait(delay, vim.schedule_wrap(function()
 				if vim.bo.filetype ~= "starter" then
@@ -52,6 +57,7 @@ return {
 				end
 				starter.refresh()
 			end), delay, true)
+			-- stylua: ignore end
 
 			return function()
 				local stats = require("lazy").stats()
@@ -60,7 +66,7 @@ return {
 			end
 		end)()
 
-		--stylua: ignore start
+		-- stylua: ignore start
 		-- This is a modified version of MiniStarter's default "recentfiles" function taken directly from the source code.
 		-- It had to be copied over because otherwise the section padding couldn't be modified to make it aligned with all other sections from the `new_section` function.
 		starter.sections.recent_files_modified = function(n, current_dir, show_path)
@@ -79,7 +85,8 @@ return {
 					current_dir and " (Current directory)" or "")
 
 				-- Use only actual readable files
-				local files = vim.tbl_filter(function(f) return vim.fn.filereadable(f) == 1 end, vim.v.oldfiles or {})
+				local files = vim.tbl_filter(function(f) return vim.fn.filereadable(f) == 1 end,
+					vim.v.oldfiles or {})
 
 				if #files == 0 then
 					return { { name = "There are no recent files (`v:oldfiles` is empty)", action = "", section = section } }
@@ -102,15 +109,21 @@ return {
 				for _, f in ipairs(vim.list_slice(files, 1, n)) do
 					local name = vim.fn.fnamemodify(f, ":t") .. show_path(f)
 					-- Added a nice `` icon for all itens compared to original source code.
-					table.insert(items, { action = "edit " .. f, name = icons.recent_files .. name, section = section })
+					table.insert(items,
+						{
+							action = "edit " .. f,
+							name = icons.recent_files .. name,
+							section =
+							    section
+						})
 				end
 
 				return items
 			end
 		end
-		--stylua: ignore end
+		-- stylua: ignore end
 
-		--stylua: ignore start
+		-- stylua: ignore start
 		-- This is modified version of MiniStarter's default "sessions" function taken directly from the source code.
 		-- It had to be copied over because otherwise the section padding couldn't be modified to make it aligned with all other sections from the `new_section` function.
 		starter.sections.sessions_modified = function(n, recent)
@@ -118,31 +131,36 @@ return {
 			if recent == nil then recent = true end
 
 			return function()
-				local section = padding .. icons.sections.session .. "Sessions" -- Added text padding and icon compared to original source code.
+				local section = padding ..
+				    icons.sections.session ..
+				    "Sessions" -- Added text padding and icon compared to original source code.
 
 				if _G.MiniSessions == nil then
-					return { { name = [['mini.sessions' is not set up]], action = '', section = section } }
+					return { { name = [[`mini.sessions` is not set up]], action = '', section = section } }
 				end
 
 				local items = {}
 				for session_name, session in pairs(_G.MiniSessions.detected) do
 					table.insert(items, {
 						_session = session,
-						name = ('%s%s'):format(session_name, session.type == 'local' and ' (local)' or ''),
+						name = ('%s%s'):format(session_name,
+							session.type == 'local' and ' (local)' or ''),
 						action = ([[lua _G.MiniSessions.read('%s')]]):format(session_name),
 						section = section,
 					})
 				end
 
 				if vim.tbl_count(items) == 0 then
-					return { { name = [[There are no detected sessions in 'mini.sessions']], action = '', section = section } }
+					return { { name = [[There are no detected sessions in `mini.sessions`]], action = '', section = section } }
 				end
 
 				local sort_fun
 				if recent then
 					sort_fun = function(a, b)
-						local a_time = a._session.type == 'local' and math.huge or a._session.modify_time
-						local b_time = b._session.type == 'local' and math.huge or b._session.modify_time
+						local a_time = a._session.type == 'local' and math.huge or
+						    a._session.modify_time
+						local b_time = b._session.type == 'local' and math.huge or
+						    b._session.modify_time
 						return a_time > b_time
 					end
 				else
@@ -161,7 +179,7 @@ return {
 				end, vim.list_slice(items, 1, n))
 			end
 		end
-		--stylua: ignore end
+		-- stylua: ignore end
 
 		starter.setup({
 			evaluate_single = true,
@@ -174,19 +192,17 @@ return {
 				starter.gen_hook.padding(6, 0),
 			},
 			items = {
-				--stylua: ignore start
-				starter.new_section(icons.actions.new_file .. "New File",    "ene | startinsert", actions_section),
-				starter.new_section(icons.actions.quit     .. "Quit Neovim", "qa!",               actions_section),
-				--stylua: ignore end
+				starter.new_section(icons.actions.new_file .. "New File", "ene | startinsert", actions_section),
+				starter.new_section(icons.actions.quit .. "Quit Neovim", "qa!", actions_section),
 
 				starter.new_section(icons.actions.file_explorer .. "Open File Explorer", function()
-					local has_neo_tree, neo_tree = pcall(require, "neo-tree")
-					local has_mini_files, mini_files = pcall(require, "mini.files")
+					local has_neotree, neo_tree = pcall(require, "neo-tree")
+					local has_minifiles, mini_files = pcall(require, "mini.files")
 
-					if has_neo_tree then
+					if has_neotree then
 						require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
 						return true
-					elseif has_mini_files then
+					elseif has_minifiles then
 						mini_files.open(vim.uv.cwd(), true)
 						return true
 					else
@@ -195,17 +211,31 @@ return {
 					end
 				end, actions_section),
 
-				--stylua: ignore start
-				starter.new_section(icons.actions.list_buffers .. "List Buffers", [[lua require("lazy").load({ plugins = "mini.pick" }); require("mini.pick").builtin.buffers()]],   actions_section),
-				starter.new_section(icons.actions.recent_files .. "Recent Files", [[lua require("lazy").load({ plugins = "mini.pick" }); require("mini.extra").pickers.oldfiles()]], actions_section),
-				starter.new_section(icons.actions.find_files .. "Find Files",   [[lua require("lazy").load({ plugins = "mini.pick" }); require("mini.pick").builtin.files()]],     actions_section),
-				starter.new_section(icons.actions.live_grep .. "Live Grep",    [[lua require("lazy").load({ plugins = "mini.pick" }); require("mini.pick").builtin.grep_live()]], actions_section),
-				starter.new_section(icons.actions.lazy .. "Lazy",         "Lazy",                                           actions_section),
-				starter.new_section(icons.actions.mason .. "Mason",        "Mason",                                          actions_section),
+				starter.new_section(
+					icons.actions.list_buffers .. "List Buffers",
+					[[lua require("lazy").load({ plugins = "mini.pick" }); require("mini.pick").builtin.buffers()]],
+					actions_section
+				),
+				starter.new_section(
+					icons.actions.recent_files .. "Recent Files",
+					[[lua require("lazy").load({ plugins = "mini.pick" }); require("mini.extra").pickers.oldfiles()]],
+					actions_section
+				),
+				starter.new_section(
+					icons.actions.find_files .. "Find Files",
+					[[lua require("lazy").load({ plugins = "mini.pick" }); require("mini.pick").builtin.files()]],
+					actions_section
+				),
+				starter.new_section(
+					icons.actions.live_grep .. "Live Grep",
+					[[lua require("lazy").load({ plugins = "mini.pick" }); require("mini.pick").builtin.grep_live()]],
+					actions_section
+				),
+				starter.new_section(icons.actions.lazy .. "Lazy", "Lazy", actions_section),
+				starter.new_section(icons.actions.mason .. "Mason", "Mason", actions_section),
 				starter.sections.recent_files_modified(5, false, false),
 				starter.sections.recent_files_modified(5, true, false),
-				starter.sections.sessions_modified(5, true)
-				--stylua: ignore end
+				starter.sections.sessions_modified(5, true),
 			},
 		})
 	end,
