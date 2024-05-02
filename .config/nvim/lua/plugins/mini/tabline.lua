b5650663ad9fdc81eda7f58a39c3127a17efafb2
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-02 19:41:31 -0300

    Icons are now disabled by default when Neovim is opened on a TTY.

--- .config/nvim/lua/plugins/mini/tabline.lua
+++ .config/nvim/lua/plugins/mini/tabline.lua
@@ -2,9 +2,16 @@ return {
 	"echasnovski/mini.tabline",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	event = { "BufReadPost", "BufNewFile", "BufUnload" },
-	opts = {
-		show_icons = true,
-		set_vim_settings = true, -- Whether to set Vim's settings for tabline (make it always shown and allow hidden buffers).
-		tabpage_section = "right", -- Where to show tabpage section in case of multiple vim tabpages.
-	},
+	opts = function()
+		local show_icons = true
+		if vim.env.DISPLAY == nil then
+			show_icons = false
+		end
+
+		return {
+			show_icons = show_icons,
+			set_vim_settings = true, -- Whether to set Vim's settings for tabline (make it always shown and allow hidden buffers).
+			tabpage_section = "right", -- Where to show tabpage section in case of multiple vim tabpages.
+		}
+	end,
 }
