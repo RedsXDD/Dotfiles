1269b0e1d143ca8e2643714bcbdef64340b9c58b
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-02 20:36:42 -0300

    Added cursor centering keymaps.
    This change allows the plugin to be properly lazy loaded on the
    "CursorMoved" event. Because before the plugin was loading with the
    pcall under keymaps.lua.

--- .config/nvim/lua/plugins/mini/animate.lua
+++ .config/nvim/lua/plugins/mini/animate.lua
@@ -1,6 +1,31 @@
 return {
 	"echasnovski/mini.animate",
 	event = "CursorMoved",
+	keys = function()
+		local M = {}
+
+		local center_map = function(keys, desc)
+			local keymap_table = {
+				keys,
+				keys .. [[<Cmd>lua require("mini.animate").execute_after("scroll", "normal! zvzz")<CR>]],
+				mode = { "n", "x" },
+				noremap = true,
+				silent = true,
+				desc = "" .. desc,
+			}
+			table.insert(M, keymap_table)
+		end
+
+		center_map("n",     "Center cursor when moving to the next match during a search.")
+		center_map("N",     "Center cursor when moving to the previous match during a search.")
+		center_map("G",     "Center cursor when moving to the last line of buffer.")
+		center_map("<C-d>", "Center cursor when moving a half page down.")
+		center_map("<C-u>", "Center cursor when moving a half page up.")
+		center_map("<C-f>", "Center cursor when moving a page down.")
+		center_map("<C-b>", "Center cursor when moving a page up.")
+
+		return M
+	end,
 	opts = function()
 		local animate = require("mini.animate")
 		local mouse_scrolled = false -- don't use animate when scrolling with the mouse.
