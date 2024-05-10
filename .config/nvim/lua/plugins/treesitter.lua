aaff13bcaf63fc0022121e93282be75bec4bf51f
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-09 22:41:52 -0300

    Fixed treesitter configuration not being required properly.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,77 +3,75 @@ return {
 	dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
 	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
-	opts = {
-		auto_install = true,
-		ensure_installed = {
-			"lua",
-			"vim",
-			"vimdoc",
-		},
-		indent = { enable = true },
-		highlight = {
-			enable = true,
-			disable = { "tmux" },
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<C-space>",
-				node_incremental = "<C-space>",
-				scope_incremental = "<C-s>",
-				node_decremental = "<C-backspace>",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			auto_install = true,
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+			indent = { enable = true },
+			highlight = {
+				enable = true,
+				disable = { "tmux" },
 			},
-		},
-		textobjects = {
-			select = {
-				enable = false,
-				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+			incremental_selection = {
+				enable = true,
 				keymaps = {
-					-- You can use the capture groups defined in textobjects.scm
-					["aa"] = "@parameter.outer",
-					["ia"] = "@parameter.inner",
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@class.outer",
-					["ic"] = "@class.inner",
-					["ii"] = "@conditional.inner",
-					["ai"] = "@conditional.outer",
-					["il"] = "@loop.inner",
-					["al"] = "@loop.outer",
-					["at"] = "@comment.outer",
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = "<C-s>",
+					node_decremental = "<C-backspace>",
 				},
 			},
-			move = {
-				enable = false,
-				set_jumps = true, -- whether to set jumps in the jumplist
-				goto_next_start = {
-					["]m"] = "@function.outer",
-					["]]"] = "@class.outer",
+			textobjects = {
+				select = {
+					enable = false,
+					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+						["ii"] = "@conditional.inner",
+						["ai"] = "@conditional.outer",
+						["il"] = "@loop.inner",
+						["al"] = "@loop.outer",
+						["at"] = "@comment.outer",
+					},
 				},
-				goto_next_end = {
-					["]M"] = "@function.outer",
-					["]["] = "@class.outer",
+				move = {
+					enable = false,
+					set_jumps = true, -- whether to set jumps in the jumplist
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]]"] = "@class.outer",
+					},
+					goto_next_end = {
+						["]M"] = "@function.outer",
+						["]["] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[["] = "@class.outer",
+					},
+					goto_previous_end = {
+						["[M"] = "@function.outer",
+						["[]"] = "@class.outer",
+					},
+					-- goto_next = {
+					--   [']i'] = "@conditional.inner",
+					-- },
+					-- goto_previous = {
+					--   ['[i'] = "@conditional.inner",
+					-- }
 				},
-				goto_previous_start = {
-					["[m"] = "@function.outer",
-					["[["] = "@class.outer",
+				swap = {
+					enable = false,
+					swap_next = { ["<leader>a"] = "@parameter.inner" },
+					swap_previous = { ["<leader>A"] = "@parameter.inner" },
 				},
-				goto_previous_end = {
-					["[M"] = "@function.outer",
-					["[]"] = "@class.outer",
-				},
-				-- goto_next = {
-				--   [']i'] = "@conditional.inner",
-				-- },
-				-- goto_previous = {
-				--   ['[i'] = "@conditional.inner",
-				-- }
 			},
-			swap = {
-				enable = false,
-				swap_next = { ["<leader>a"] = "@parameter.inner" },
-				swap_previous = { ["<leader>A"] = "@parameter.inner" },
-			},
-		},
-	},
+		})
+	end,
 }
