a53900bc60c40880364dfe8a6de0b2cfc68c1242
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-10 13:21:29 -0300

    Added configuration options for neopywal.

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -18,7 +18,25 @@ return {
 		name = "neopywal",
 		lazy = false,
 		priority = 1000,
-		opts = {},
+		opts = {
+			default_fileformats = false,
+			default_plugins = false,
+			plugins = {
+				treesitter = true,
+				lspconfig = true,
+				nvim_cmp = true,
+				mini = {
+					cursorword = true,
+					files = true,
+					hipatterns = true,
+					indentscope = true,
+					pick = true,
+					starter = true,
+					statusline = true,
+					tabline = true,
+				},
+			},
+		},
 		config = function()
 			if vim.env.DISPLAY ~= nil then
 				vim.cmd.colorscheme("neopywal")
