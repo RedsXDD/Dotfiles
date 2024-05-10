b2399fe55855a1ee2ea64f41771457e531d65fd4
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-09 22:41:30 -0300

    Installed catppuccin and tokyonight colorschemes.

---
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,31 @@
+return {
+	{
+		"catppuccin/nvim",
+		lazy = true,
+		name = "catppuccin",
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"RedsXDD/neopywal.nvim",
+		branch = "testing",
+		name = "neopywal",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			if vim.env.DISPLAY ~= nil then
+				vim.cmd.colorscheme("neopywal")
+			else
+				vim.cmd([[set notermguicolors]])
+				vim.cmd.colorscheme("default")
+			end
+		end,
+	},
+}
