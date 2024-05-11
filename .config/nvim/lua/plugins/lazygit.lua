255df1113839c0463aabfe7206f3a0aac7945f91
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-11 16:05:57 -0300

    Installed lazygit.nvim.

---
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,14 @@
+return {
+	"kdheepak/lazygit.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	cmd = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
+	},
+	keys = {
+		{ "<leader>gl", "<cmd>LazyGit<cr>", desc = "Open LazyGit." },
+	}
+}
