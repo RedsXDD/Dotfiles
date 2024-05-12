822efbb4ccc3d18c0d5aa8e1776488817b2b641d
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-11 23:29:40 -0300

    Changed default options for neopywal.nvim.

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -17,7 +17,27 @@ return {
 		name = "neopywal",
 		lazy = false,
 		priority = 1000,
-		opts = { default_fileformats = false },
+		opts = {
+			default_fileformats = false,
+			default_plugins = false,
+			plugins = {
+				nvim_cmp = true,
+				lazy = true,
+				lspconfig = true,
+				neotree = true,
+				treesitter = true,
+				mini = {
+					cursorword = true,
+					files = true,
+					hipatterns = true,
+					indentscope = true,
+					pick = true,
+					starter = true,
+					statusline = true,
+					tabline = true,
+				},
+			},
+		},
 		config = function(_, opts)
 			local neopywal = require("neopywal")
 			neopywal.setup(opts)
