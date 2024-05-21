ada43c38590a85bc3947d5ae354fcdb084028e63
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-21 18:24:47 -0300

    Changed Dollar styling and added search commands.

--- .librewolf/default/chrome/Dollar/src/js/commands.js
+++ .librewolf/default/chrome/Dollar/src/js/commands.js
@@ -1,28 +1,57 @@
-// Search something on google, if no arguments are provided => www.google.com
-g = google;
-function google(args) {
-    if (args != undefined) {
-        search = args.replace(" ", "+")
-        window.open("https://www.google.com/search?q=" + search);
-    } else {
-        window.open("https://www.google.com");
-    }
-}
-
 // Display time
 time = clock;
 function clock(args) {
-    var today = new Date();
-    block_log(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
+	var today = new Date();
+	block_log(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
 }
 
 // Clear the terminal
-cls = clear;
+cl = clear;
 function clear(args) {
-    document.getElementById('wrapper').innerHTML = "";
+	document.getElementById('wrapper').innerHTML = "";
 }
 
+// Search something on searxng, if no arguments are provided => https://searxng.ca
+s = searxng;
+function searxng(args) {
+	instance = "https://searxng.ca"
+	if (args != undefined) {
+		search = args.replace(" ", "+")
+		window.open(instance + "/search?q=" + search);
+	} else {
+		window.open(instance);
+	}
+}
 
+// Search something on brave, if no arguments are provided => https://search.brave.com
+b = brave;
+function brave(args) {
+	if (args != undefined) {
+		search = args.replace(" ", "+")
+		window.open("https://search.brave.com/search?q=" + search);
+	} else {
+		window.open("https://search.brave.com");
+	}
+}
 
+// Search something on duckduckgo, if no arguments are provided => https://duckduckgo.com
+d = duckduckgo;
+function duckduckgo(args) {
+	if (args != undefined) {
+		search = args.replace(" ", "+")
+		window.open("https://duckduckgo.com/search?q=" + search);
+	} else {
+		window.open("https://duckduckgo.com");
+	}
+}
 
-
+// Search something on google, if no arguments are provided => https://www.google.com
+g = google;
+function google(args) {
+	if (args != undefined) {
+		search = args.replace(" ", "+")
+		window.open("https://www.google.com/search?q=" + search);
+	} else {
+		window.open("https://www.google.com");
+	}
+}
--- .librewolf/default/chrome/Dollar/src/js/terminal.js
+++ .librewolf/default/chrome/Dollar/src/js/terminal.js
@@ -1,6 +1,6 @@
 // Terminal config
 config = {
-    shellPrompt: "$ "
+    shellPrompt: "❯ "
 }
 
 document.getElementById("input_title").innerText = config.shellPrompt;
