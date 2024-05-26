09301245dc5707e75c52ff6f94f71c634c2f3dec
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-26 11:36:45 -0300

    Added rust.vim formatter plugin.

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,14 +1,21 @@
 return {
-        {
-                "theRealCarneiro/hyprland-vim-syntax",
-                ft = "hypr",
-        },
-        {
-                "camnw/lf-vim",
-                ft = "lf",
-        },
+	{
+		"theRealCarneiro/hyprland-vim-syntax",
+		ft = "hypr",
+	},
+	{
+		"camnw/lf-vim",
+		ft = "lf",
+	},
 	{
 		"fladson/vim-kitty",
-                ft = "kitty",
+		ft = "kitty",
+	},
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
 	},
 }
