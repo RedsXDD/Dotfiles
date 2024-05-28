a46ed25deb7316d34ebf52bfb0177987564700b6
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-28 16:30:38 -0300

    Improved neopywal CmpItemKind highlights.

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -23,34 +23,35 @@ return {
 			default_fileformats = false,
 			default_plugins = true,
 			custom_highlights = function(colors)
+				local U = require("neopywal.util")
 				return {
-					CmpItemKindSnippet = { bg = colors.color5, fg = colors.background },
-					CmpItemKindKeyword = { bg = colors.color1, fg = colors.background },
-					CmpItemKindText = { bg = colors.color6, fg = colors.background },
-					CmpItemKindMethod = { bg = colors.color4, fg = colors.background },
-					CmpItemKindConstructor = { bg = colors.color4, fg = colors.background },
-					CmpItemKindFunction = { bg = colors.color4, fg = colors.background },
-					CmpItemKindFolder = { bg = colors.color4, fg = colors.background },
-					CmpItemKindModule = { bg = colors.color4, fg = colors.background },
-					CmpItemKindConstant = { bg = colors.color11, fg = colors.background },
-					CmpItemKindField = { bg = colors.color2, fg = colors.background },
-					CmpItemKindProperty = { bg = colors.color2, fg = colors.background },
-					CmpItemKindEnum = { bg = colors.color2, fg = colors.background },
-					CmpItemKindUnit = { bg = colors.color2, fg = colors.background },
-					CmpItemKindClass = { bg = colors.color3, fg = colors.background },
-					CmpItemKindVariable = { bg = colors.color3, fg = colors.background },
-					CmpItemKindFile = { bg = colors.color4, fg = colors.background },
-					CmpItemKindInterface = { bg = colors.color3, fg = colors.background },
-					CmpItemKindColor = { bg = colors.color1, fg = colors.background },
-					CmpItemKindReference = { bg = colors.color1, fg = colors.background },
-					CmpItemKindEnumMember = { bg = colors.color1, fg = colors.background },
-					CmpItemKindStruct = { bg = colors.color4, fg = colors.background },
-					CmpItemKindValue = { bg = colors.color11, fg = colors.background },
-					CmpItemKindEvent = { bg = colors.color4, fg = colors.background },
-					CmpItemKindOperator = { bg = colors.color4, fg = colors.background },
-					CmpItemKindTypeParameter = { bg = colors.color4, fg = colors.background },
-					CmpItemKindCopilot = { bg = colors.color6, fg = colors.background },
-					CmpItemKindTabNine = { bg = colors.color6, fg = colors.background },
+					CmpItemKindKeyword = { bg = colors.color1, fg = colors.background }, -- Link Keyword
+					CmpItemKindOperator = { link = "CmpItemKindKeyword" }, -- Link Operator
+					CmpItemKindEnum = { link = "CmpItemKindKeyword" },
+					CmpItemKindFunction = { bg = colors.color2, fg = colors.background }, -- Link Function
+					CmpItemKindField = { link = "CmpItemKindFunction" }, -- Link @variable.member
+					CmpItemKindProperty = { link = "CmpItemKindFunction" }, -- Link @property
+					CmpItemKindMethod = { link = "CmpItemKindFunction" },
+					CmpItemKindColor = { bg = colors.color3, fg = colors.background },
+					CmpItemKindInterface = { bg = colors.color4, fg = colors.background },
+					CmpItemKindFolder = { link = "CmpItemKindInterface" }, -- Link Directory
+					CmpItemKindVariable = { link = "CmpItemKindInterface" }, -- Link Variable
+					CmpItemKindEvent = { link = "CmpItemKindVariable" },
+					CmpItemKindConstructor = { bg = colors.color5, fg = colors.background }, -- Link Special
+					CmpItemKindModule = { link = "CmpItemKindConstructor" }, -- Link Include
+					CmpItemKindUnit = { link = "CmpItemKindConstructor" }, -- Link Number
+					CmpItemKindSnippet = { link = "CmpItemKindModule" },
+					CmpItemKindClass   = { bg = colors.color6, fg = colors.background }, -- Link StorageClass
+					CmpItemKindStruct  = { link = "CmpItemKindClass" }, -- Link Structure
+					CmpItemKindCopilot = { link = "CmpItemKindClass" },
+					CmpItemKindTabNine = { link = "CmpItemKindClass" },
+					CmpItemKindTypeParameter = { bg = colors.color11, fg = colors.background }, -- Link Identifier
+					CmpItemKindConstant = { link = "CmpItemKindTypeParameter" }, -- Link Constant
+					CmpItemKindValue = { link = "CmpItemKindTypeParameter" },
+					CmpItemKindEnumMember = { link = "CmpItemKindTypeParameter" },
+					CmpItemKindText = { bg = U.lighten(colors.background, 0.3), fg = colors.foreground },
+					CmpItemKindFile = { link = "CmpItemKindText" },
+					CmpItemKindReference = { link = "CmpItemKindText" },
 				}
 			end,
 		},
