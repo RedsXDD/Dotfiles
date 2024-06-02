7e9be0b7a44a7c4a232e664212d66deb591bb27f
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-06-02 13:43:03 -0300

    refactor(shell/profile): Formatted file and added better fzf wallust theming.

---
+++ .config/shell/fzf_wallust.sh
@@ -0,0 +1,16 @@
+export FZF_DEFAULT_OPTS="\
+	${FZF_DEFAULT_OPTS} \
+	--color=bg:-1,bg+:#32344A,fg:-1,fg+:-1 \
+	--color=hl:#7AA2F7,hl+:#7AA2F7,info:#A9B1D6,marker:#9ECE6A \
+	--color=prompt:#449DAB,spinner:#FF9E64,pointer:#449DAB,header:#E0AF68 \
+	--color=border:#A9B1D6,label:#449DAB,query:#A9B1D6 \
+	--border='rounded' \
+	--border-label='' \
+	--preview-window='border-rounded' \
+	--padding='1' \
+	--prompt='> ' \
+	--marker='>' \
+	--pointer='◆' \
+	--separator='─' \
+	--no-scrollbar \
+	--layout='reverse'"
