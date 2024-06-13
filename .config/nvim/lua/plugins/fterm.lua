105796c755658d419a3bacfebffa395e7a5ecfac
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-06-13 00:48:01 -0300

    feat(nvim/plugins): Added FTerm.nvim.

---
+++ .config/nvim/lua/plugins/fterm.lua
@@ -0,0 +1,44 @@
+return {
+	"numToStr/FTerm.nvim",
+	keys = function()
+		local M = {}
+		local fterm = require("FTerm")
+
+		local term_map = function(keys, func, desc)
+			local keymap_table = { keys, func, mode = { "n" }, noremap = true, desc = "" .. desc }
+			table.insert(M, keymap_table)
+		end
+
+		term_map("<Leader>gt", function()
+			fterm.toggle()
+		end, "Open a floating terminal.")
+
+		term_map("<Leader>gg", function()
+			local gitui = fterm:new({
+				ft = "fterm_gitui", -- You can also override the default filetype, if you want
+				cmd = { "gitui", "-d", vim.api.nvim_buf_get_name(0) },
+				blend = 0,
+				dimensions = {
+					height = 0.9,
+					width = 0.9,
+				},
+			})
+			gitui:toggle()
+		end, "Open Gitui.")
+
+		return M
+	end,
+	opts = {
+		ft = "FTerm",
+		border = require("user.icons").icons.misc.border,
+		auto_close = true,
+		hl = "NormalFloat",
+		blend = 0,
+		dimensions = {
+			height = 0.9, -- Height of the terminal window
+			width = 0.9, -- Width of the terminal window
+			x = 0.5, -- X axis of the terminal window
+			y = 0.5, -- Y axis of the terminal window
+		},
+	},
+}
