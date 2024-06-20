085d291e85aa2ab0ecf9cd66b3dfb3138f8feaa5
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-06-20 13:34:31 -0300

    refactor(nvim/mini.surround): remove `<Leader>`
    This commit removes the `<Leader>` key prefix for the surround
    keymappings.

--- .config/nvim/lua/plugins/mini/surround.lua
+++ .config/nvim/lua/plugins/mini/surround.lua
@@ -1,24 +1,15 @@
 return {
 	"echasnovski/mini.surround",
 	keys = {
-		{ "<Leader>sa", mode = { "n", "v" }, desc = "Add Surrounding." },
-		{ "<Leader>sd", desc = "Delete Surrounding." },
-		{ "<Leader>sr", desc = "Replace Surrounding." },
-		{ "<Leader>sf", desc = "Find Right Surrounding." },
-		{ "<Leader>sF", desc = "Find Left Surrounding." },
-		{ "<Leader>sh", desc = "Highlight Surrounding." },
-		{ "<Leader>sn", desc = "Update `MiniSurround.config.n_lines`." },
+		{ "sa", mode = { "n", "v" }, desc = "Add Surrounding." },
+		{ "sd", desc = "Delete Surrounding." },
+		{ "sr", desc = "Replace Surrounding." },
+		{ "sf", desc = "Find Right Surrounding." },
+		{ "sF", desc = "Find Left Surrounding." },
+		{ "sh", desc = "Highlight Surrounding." },
+		{ "sn", desc = "Update `MiniSurround.config.n_lines`." },
 	},
 	opts = {
 		respect_selection_type = true,
-		mappings = {
-			add = "<Leader>sa", -- Add surrounding in Normal and Visual modes.
-			delete = "<Leader>sd", -- Delete surrounding.
-			find = "<Leader>sf", -- Find surrounding (to the right).
-			find_left = "<Leader>sF", -- Find surrounding (to the left).
-			highlight = "<Leader>sh", -- Highlight surrounding.
-			replace = "<Leader>sr", -- Replace surrounding.
-			update_n_lines = "<Leader>sn", -- Update `n_lines`.
-		},
 	},
 }
