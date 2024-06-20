3bd5c9cf9f688166cd34498ff6d6f142c666b570
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-06-20 14:22:48 -0300

    refactor(nvim/bufferline): remove unneded options
    This commit removes bufferline options that are already set by default
    with the values that have been "modified".

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -7,20 +7,13 @@ return {
 		return {
 			highlights = require("neopywal.bufferline").setup(),
 			options = {
-				mode = "buffers",
-				numbers = "ordinal", -- "none" | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-				close_command = "bdelete! %d", -- can be a string | function, | false see "Mouse actions"
+				numbers = "ordinal",
 				buffer_close_icon = "",
 				close_icon = "",
 				modified_icon = "●",
 				left_trunc_marker = "",
 				right_trunc_marker = "",
-				-- max_name_length = 20,
-				max_prefix_length = 15,
-				truncate_names = false,
-				tab_size = 10,
 				diagnostics = "nvim_lsp",
-				always_show_bufferline = true,
 				separator_style = "slope", -- "slant" | "slope" | "thick" | "thin" | { 'any', 'any' }
 				offsets = {
 					{
