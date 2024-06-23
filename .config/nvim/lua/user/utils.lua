84e0da4d9b17aadfd6553e91ab1124f7683ce9ea
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-06-22 21:36:36 -0300

    feat(nvim/utils): add file explorer functions
    This commit adds both `toggle_netrw` and `toggle_file_explorer` from
    keymaps.lua into utils.lua.

--- .config/nvim/lua/user/utils.lua
+++ .config/nvim/lua/user/utils.lua
@@ -39,4 +39,51 @@ function M.maximize(state)
 	})
 end
 
+function M.toggle_netrw()
+	pcall(require, "netrw") -- Try loading netrw.nvim if it's installed.
+
+	local netrw_winsize = vim.g.netrw_winsize or 30
+
+	local bufnr = vim.api.nvim_get_current_buf()
+	local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+
+	if vim.g.netrw_is_open and filetype == "netrw" then
+		vim.api.nvim_buf_delete(0, { force = true })
+		vim.g.netrw_is_open = false
+	else
+		vim.g.netrw_is_open = true
+		vim.cmd("Lexplore")
+		vim.cmd("vertical resize " .. netrw_winsize)
+	end
+end
+
+---@param chdir boolean?
+function M.toggle_file_explorer(chdir)
+	local has_neotree, neotree = pcall(require, "neo-tree.command")
+	local has_minifiles, files = pcall(require, "mini.files")
+
+	if type(chdir) == "boolean" and chdir == true then
+		local filepath = vim.api.nvim_buf_get_name(0)
+		local current_directory = vim.fs.dirname(filepath)
+		vim.fn.chdir(current_directory)
+	end
+
+	local toggle_mini_files = function(path, use_lastest)
+		if not files.close() then
+			files.open(path, use_lastest)
+		end
+	end
+
+	---@diagnostic disable-next-line: undefined-field
+	local directory_path = vim.uv.cwd()
+
+	if has_neotree then
+		neotree.execute({ toggle = true, dir = directory_path })
+	elseif has_minifiles then
+		toggle_mini_files(directory_path, true)
+	else
+		M.toggle_netrw()
+	end
+end
+
 return M
