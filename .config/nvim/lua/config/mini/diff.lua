b9a574776744fca82c449df937b7ea64bf900668
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-06-23 14:02:48 -0300

    refactor(nvim/mini.diff): remove unmodified opts

--- .config/nvim/lua/config/mini/diff.lua
+++ .config/nvim/lua/config/mini/diff.lua
@@ -2,7 +2,6 @@ local diff = require("mini.diff")
 local icons = require("user.icons").gitsigns
 
 diff.setup({
-	delay = { text_change = 200 }, -- How much to wait before update following every text change.
 	view = {
 		style = "sign",
 		signs = {
@@ -16,17 +15,5 @@ diff.setup({
 		textobject = "<Leader>dh", -- Hunk range textobject to be used inside operator.
 		apply = "<Leader>dh", -- Apply hunks inside a visual/operator region.
 		reset = "<Leader>dH", -- Reset hunks inside a visual/operator region.
-
-		-- Go to hunk range in corresponding direction.
-		goto_first = "[H",
-		goto_prev = "[h",
-		goto_next = "]h",
-		goto_last = "]H",
-	},
-	options = {
-		algorithm = "histogram",
-		indent_heuristic = true,
-		linematch = 60,
-		wrap_goto = false,
 	},
 })
