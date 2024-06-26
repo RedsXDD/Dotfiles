0ed78bf8b5d71aa5cdb9572162e99a5f510ff45b
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-06-26 20:28:06 -0300

    feat(nvim/mini.splijoin): <Leader>j as prefix
    This commit changes the keymappings for `mini.splitjoin` by making
    "<Leader>j" as the prefix for each keymap action. This change allows to
    easily add keymaps for the split/join actions without having to worry
    about possible conficts with other keymappings and also allows for
    better integration with plugins like `which-key` or `mini.clue` since
    `<Leader>j` now is it's own group of keymappings.

--- .config/nvim/lua/config/mini/splitjoin.lua
+++ .config/nvim/lua/config/mini/splitjoin.lua
@@ -3,8 +3,8 @@ local splitjoin = require("mini.splitjoin")
 splitjoin.setup({
 	detect = { separator = "[,;]" },
 	mappings = {
-		toggle = "gS",
-		split = "",
-		join = "",
+		toggle = "<Leader>jt",
+		split = "<Leader>js",
+		join = "<Leader>jj",
 	},
 })
