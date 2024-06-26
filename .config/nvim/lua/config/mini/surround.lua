bd6ea0681cb00332805ea4d7c6f5de6299c0ba08
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-06-26 20:26:52 -0300

    feat(nvim/mini.surround): <Leader>s as prefix
    This commit changes the keymappings for `mini.surround` by making
    "<Leader>s" the prefix for each keymap action. This avoids conficts
    with the builtin "s" keymapping from Neovim and also allows for better
    integration with plugins like `which-key` or `mini.clue` since
    `<Leader>s` now is it's own group of keymappings.

--- .config/nvim/lua/config/mini/surround.lua
+++ .config/nvim/lua/config/mini/surround.lua
@@ -2,4 +2,13 @@ local surround = require("mini.surround")
 
 surround.setup({
 	respect_selection_type = true,
+	mappings = {
+		add = "<Leader>sa", -- Add surrounding in Normal and Visual modes
+		delete = "<Leader>sd", -- Delete surrounding
+		find = "<Leader>sf", -- Find surrounding (to the right)
+		find_left = "<Leader>sF", -- Find surrounding (to the left)
+		highlight = "<Leader>sh", -- Highlight surrounding
+		replace = "<Leader>sr", -- Replace surrounding
+		update_n_lines = "<Leader>sn", -- Update `n_lines`
+	},
 })
