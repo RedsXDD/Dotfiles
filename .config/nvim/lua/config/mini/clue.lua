6f7e0d7535c0c3b0445a503afc3dc19db9467db8
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-06-26 19:05:01 -0300

    refactor(nvim/mini.clue): organize clues table
    This commit improves the code organization inside the `clues` table by
    adding vim folds and grouping similar plugin clues together.

--- .config/nvim/lua/config/mini/clue.lua
+++ .config/nvim/lua/config/mini/clue.lua
@@ -1,3 +1,5 @@
+-- vim:fileencoding=utf-8:foldmethod=marker
+
 local clue = require("mini.clue")
 local border_style = require("core.icons").misc.border
 
@@ -48,24 +50,36 @@ clue.setup({
 		{ mode = "x", keys = "]" },
 	},
 	clues = {
-		-- General:
+		--: Builtin {{{
+		clue.gen_clues.builtin_completion(),
+		clue.gen_clues.g(),
+		clue.gen_clues.marks(),
+		clue.gen_clues.registers(),
+		clue.gen_clues.z(),
+		clue.gen_clues.windows({
+			submode_move = true,
+			submode_navigate = true,
+			submode_resize = true,
+		}),
+		--: }}}
+		--: General {{{
 		{ mode = "n", keys = "<Leader>b", desc = "+Buffers & Tabs" },
 		{ mode = "x", keys = "<Leader>b", desc = "+Buffers & Tabs" },
 		{ mode = "n", keys = "<Leader>t", desc = "+Toggles" },
 		{ mode = "x", keys = "<Leader>t", desc = "+Toggles" },
 		{ mode = "n", keys = "<Leader>g", desc = "+Misc" },
 		{ mode = "x", keys = "<Leader>g", desc = "+Misc" },
-
-		-- Zenmode:
+		--: }}}
+		--: Zenmode {{{
 		mkclue_with_deps("zen-mode", {
 			{ mode = "n", keys = "<Leader>tz", desc = "Toggle Zenmode." },
 			{ mode = "x", keys = "<Leader>tz", desc = "Toggle Zenmode." },
 		}),
-
-		-- Noice:
+		--: }}}
+		--: Noice {{{
 		mkclue_with_deps("noice", { mode = "n", keys = "<Leader>gn", desc = "+Noice" }),
-
-		-- Lsp:
+		--: }}}
+		--: Lsp {{{
 		mkclue_with_deps("lspconfig", {
 			{ mode = "n", keys = "<Leader>l", desc = "+LSP" },
 			{ mode = "n", keys = "]d", postkeys = "]" },
@@ -77,40 +91,16 @@ clue.setup({
 			{ mode = "x", keys = "[d", postkeys = "[" },
 			{ mode = "x", keys = "[D", postkeys = "[" },
 		}),
-
-		-- Mini.map:
-		mkclue_with_deps("mini.map", { mode = "n", keys = "<Leader>m", desc = "+MiniMap" }),
-
-		-- Mini.diff:
-		mkclue_with_deps("mini.diff", {
-			{ mode = "n", keys = "<Leader>d", desc = "+MiniDiff" },
-			{ mode = "n", keys = "]h", postkeys = "]" },
-			{ mode = "n", keys = "]H", postkeys = "]" },
-			{ mode = "n", keys = "[h", postkeys = "[" },
-			{ mode = "n", keys = "[H", postkeys = "[" },
-			{ mode = "x", keys = "]h", postkeys = "]" },
-			{ mode = "x", keys = "]H", postkeys = "]" },
-			{ mode = "x", keys = "[h", postkeys = "[" },
-			{ mode = "x", keys = "[H", postkeys = "[" },
-		}),
-
-		-- Mini.pick:
-		mkclue_with_deps("mini.pick", {
-			{ mode = "n", keys = "<Leader>f", desc = "+MiniPick" },
-			{ mode = "n", keys = "<Leader>fg", desc = "+Grep & Git" },
-			{ mode = "n", keys = "<Leader>fl", desc = "+LSP" },
-			{ mode = "n", keys = "<Leader>fe", desc = "+Extra" },
-		}),
-
-		-- Telescope:
+		--: }}}
+		--: Telescope {{{
 		mkclue_with_deps("telescope", {
 			{ mode = "n", keys = "<Leader>f", desc = "+Telescope" },
 			{ mode = "n", keys = "<Leader>fg", desc = "+Grep & Git" },
 			{ mode = "n", keys = "<Leader>fl", desc = "+LSP" },
 			{ mode = "n", keys = "<Leader>fe", desc = "+Extra" },
 		}),
-
-		-- Gitsigns:
+		--: }}}
+		--: Gitsigns {{{
 		mkclue_with_deps("gitsigns", {
 			{ mode = "n", keys = "<Leader>gh", desc = "+Gitsigns" },
 			{ mode = "x", keys = "<Leader>gh", desc = "+Gitsigns" },
@@ -123,8 +113,8 @@ clue.setup({
 			{ mode = "x", keys = "[h", postkeys = "[" },
 			{ mode = "x", keys = "[H", postkeys = "[" },
 		}),
-
-		-- Jump betwheen brackets.
+		--: }}}
+		--: Jump betwheen brackets {{{
 		{ mode = "n", keys = "])", desc = "+Jump to next `)`" },
 		{ mode = "n", keys = "]]", desc = "+Jump to next `]`" },
 		{ mode = "n", keys = "]}", desc = "+Jump to next `}`" },
@@ -137,16 +127,40 @@ clue.setup({
 		{ mode = "x", keys = "[(", desc = "+Jump to previous `(`" },
 		{ mode = "x", keys = "[[", desc = "+Jump to previous `[`" },
 		{ mode = "x", keys = "[{", desc = "+Jump to previous `{`" },
-
-		-- Mini.bracketed:
+		--: }}}
+		--: Mini.map {{{
+		mkclue_with_deps("mini.map", { mode = "n", keys = "<Leader>m", desc = "+MiniMap" }),
+		--: }}}
+		--: Mini.diff {{{
+		mkclue_with_deps("mini.diff", {
+			{ mode = "n", keys = "<Leader>d", desc = "+MiniDiff" },
+			{ mode = "n", keys = "]h", postkeys = "]" },
+			{ mode = "n", keys = "]H", postkeys = "]" },
+			{ mode = "n", keys = "[h", postkeys = "[" },
+			{ mode = "n", keys = "[H", postkeys = "[" },
+			{ mode = "x", keys = "]h", postkeys = "]" },
+			{ mode = "x", keys = "]H", postkeys = "]" },
+			{ mode = "x", keys = "[h", postkeys = "[" },
+			{ mode = "x", keys = "[H", postkeys = "[" },
+		}),
+		--: }}}
+		--: Mini.pick {{{
+		mkclue_with_deps("mini.pick", {
+			{ mode = "n", keys = "<Leader>f", desc = "+MiniPick" },
+			{ mode = "n", keys = "<Leader>fg", desc = "+Grep & Git" },
+			{ mode = "n", keys = "<Leader>fl", desc = "+LSP" },
+			{ mode = "n", keys = "<Leader>fe", desc = "+Extra" },
+		}),
+		--: }}}
+		--: Mini.bracketed {{{
 		mkclue_with_deps("mini.bracketed", {
-			-- Jump betwheen buffers.
+			--: Jump betwheen buffers {{{
 			{ mode = "n", keys = "]b", postkeys = "]" },
 			{ mode = "n", keys = "]B", postkeys = "]" },
 			{ mode = "n", keys = "[b", postkeys = "[" },
 			{ mode = "n", keys = "[B", postkeys = "[" },
-
-			-- Jump betwheen comments.
+			--: }}}
+			--: Jump betwheen comments {{{
 			{ mode = "n", keys = "]c", postkeys = "]" },
 			{ mode = "n", keys = "]C", postkeys = "]" },
 			{ mode = "n", keys = "[c", postkeys = "[" },
@@ -155,8 +169,8 @@ clue.setup({
 			{ mode = "x", keys = "]C", postkeys = "]" },
 			{ mode = "x", keys = "[c", postkeys = "[" },
 			{ mode = "x", keys = "[C", postkeys = "[" },
-
-			-- Jump betwheen conflicts.
+			--: }}}
+			--: Jump betwheen conflicts {{{
 			{ mode = "n", keys = "]x", postkeys = "]" },
 			{ mode = "n", keys = "]X", postkeys = "]" },
 			{ mode = "n", keys = "[x", postkeys = "[" },
@@ -165,14 +179,14 @@ clue.setup({
 			{ mode = "x", keys = "]X", postkeys = "]" },
 			{ mode = "x", keys = "[x", postkeys = "[" },
 			{ mode = "x", keys = "[X", postkeys = "[" },
-
-			-- Jump betwheen files.
+			--: }}}
+			--: Jump betwheen files {{{
 			{ mode = "n", keys = "]f", postkeys = "]" },
 			{ mode = "n", keys = "]F", postkeys = "]" },
 			{ mode = "n", keys = "[f", postkeys = "[" },
 			{ mode = "n", keys = "[F", postkeys = "[" },
-
-			-- Jump betwheen indentation.
+			--: }}}
+			--: Jump betwheen indentation {{{
 			{ mode = "n", keys = "]i", postkeys = "]" },
 			{ mode = "n", keys = "]I", postkeys = "]" },
 			{ mode = "n", keys = "[i", postkeys = "[" },
@@ -181,32 +195,32 @@ clue.setup({
 			{ mode = "x", keys = "]I", postkeys = "]" },
 			{ mode = "x", keys = "[i", postkeys = "[" },
 			{ mode = "x", keys = "[I", postkeys = "[" },
-
-			-- Jump betwheen jumps from jump-list.
+			--: }}}
+			--: Jump betwheen jumps from jump-list {{{
 			{ mode = "n", keys = "]j", postkeys = "]" },
 			{ mode = "n", keys = "]J", postkeys = "]" },
 			{ mode = "n", keys = "[j", postkeys = "[" },
 			{ mode = "n", keys = "[J", postkeys = "[" },
-
-			-- Jump betwheen locations from location-list.
+			--: }}}
+			--: Jump betwheen locations from location-list {{{
 			{ mode = "n", keys = "]l", postkeys = "]" },
 			{ mode = "n", keys = "]L", postkeys = "]" },
 			{ mode = "n", keys = "[l", postkeys = "[" },
 			{ mode = "n", keys = "[L", postkeys = "[" },
-
-			-- Jump betwheen old files.
+			--: }}}
+			--: Jump betwheen old files {{{
 			{ mode = "n", keys = "]o", postkeys = "]" },
 			{ mode = "n", keys = "]O", postkeys = "]" },
 			{ mode = "n", keys = "[o", postkeys = "[" },
 			{ mode = "n", keys = "[O", postkeys = "[" },
-
-			-- Jump betwheen quickfixes from quickfix-list.
+			--: }}}
+			--: Jump betwheen quickfixes from quickfix-list {{{
 			{ mode = "n", keys = "]q", postkeys = "]" },
 			{ mode = "n", keys = "]Q", postkeys = "]" },
 			{ mode = "n", keys = "[q", postkeys = "[" },
 			{ mode = "n", keys = "[Q", postkeys = "[" },
-
-			-- Jump betwheen tree-sitter node and parents.
+			--: }}}
+			--: Jump betwheen tree-sitter node and parents {{{
 			{ mode = "n", keys = "]t", postkeys = "]" },
 			{ mode = "n", keys = "]T", postkeys = "]" },
 			{ mode = "n", keys = "[t", postkeys = "[" },
@@ -215,35 +229,26 @@ clue.setup({
 			{ mode = "x", keys = "]T", postkeys = "]" },
 			{ mode = "x", keys = "[t", postkeys = "[" },
 			{ mode = "x", keys = "[T", postkeys = "[" },
-
-			-- Jump betwheen undo states.
+			--: }}}
+			--: Jump betwheen undo states {{{
 			{ mode = "n", keys = "]u", postkeys = "]" },
 			{ mode = "n", keys = "]U", postkeys = "]" },
 			{ mode = "n", keys = "[u", postkeys = "[" },
 			{ mode = "n", keys = "[U", postkeys = "[" },
-
-			-- Jump betwheen windows.
+			--: }}}
+			--: Jump betwheen windows {{{
 			{ mode = "n", keys = "]w", postkeys = "]" },
 			{ mode = "n", keys = "]W", postkeys = "]" },
 			{ mode = "n", keys = "[w", postkeys = "[" },
 			{ mode = "n", keys = "[W", postkeys = "[" },
-
-			-- Jump betwheen yank selection replacing latest put region.
+			--: }}}
+			--: Jump betwheen yank selection replacing latest put region {{{
 			{ mode = "n", keys = "]y", postkeys = "]" },
 			{ mode = "n", keys = "]Y", postkeys = "]" },
 			{ mode = "n", keys = "[y", postkeys = "[" },
 			{ mode = "n", keys = "[Y", postkeys = "[" },
+			--: }}}
 		}),
-
-		clue.gen_clues.builtin_completion(),
-		clue.gen_clues.g(),
-		clue.gen_clues.marks(),
-		clue.gen_clues.registers(),
-		clue.gen_clues.z(),
-		clue.gen_clues.windows({
-			submode_move = true,
-			submode_navigate = true,
-			submode_resize = true,
-		}),
+		--: }}}
 	},
 })
