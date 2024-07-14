d176074d86c3d662af967c7b1249ca16fb42a566
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-07-13 21:57:08 -0300

    refactor(nvim/ftplugin): use local options

--- .config/nvim/ftplugin/gitcommit.lua
+++ .config/nvim/ftplugin/gitcommit.lua
@@ -1,8 +1,8 @@
-vim.opt.colorcolumn = { "50", "72" }
-vim.opt.textwidth = 72
-vim.opt.formatoptions:append("t")
-vim.opt.softtabstop = -1
-vim.opt.shiftwidth = 0
-vim.opt.tabstop = 2
-vim.opt.expandtab = true
-vim.opt.autoindent = true
+vim.opt_local.colorcolumn = { "50", "72" }
+vim.opt_local.textwidth = 72
+vim.opt_local.formatoptions:append("t")
+vim.opt_local.softtabstop = -1
+vim.opt_local.shiftwidth = 0
+vim.opt_local.tabstop = 2
+vim.opt_local.expandtab = true
+vim.opt_local.autoindent = true
--- .config/nvim/ftplugin/toml.lua
+++ .config/nvim/ftplugin/toml.lua
@@ -1,5 +1,5 @@
-vim.opt.softtabstop = -1
-vim.opt.shiftwidth = 0
-vim.opt.tabstop = 2
-vim.opt.expandtab = true
-vim.opt.autoindent = true
+vim.opt_local.softtabstop = -1
+vim.opt_local.shiftwidth = 0
+vim.opt_local.tabstop = 2
+vim.opt_local.expandtab = true
+vim.opt_local.autoindent = true
