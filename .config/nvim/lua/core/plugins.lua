95e31edb0691efcd4c2ded9bef2feaf7e8d8f7d7
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-07-17 00:28:32 -0300

    feat(nvim/plugins): add flash.nvim

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -579,4 +579,19 @@ return {
 		config = load_config("telescope"),
 	},
 	--: }}}
+	--: flash.nvim {{{
+	{
+		"folke/flash.nvim",
+		event = "LazyFile",
+		opts = {},
+                -- stylua: ignore
+                keys = {
+                        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+                        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+                        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+                        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+                        { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+                },
+	},
+	--: }}}
 }
