ff3edaf99fed2a2d15329f09c81a4cef865a0aba
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-07-22 20:17:10 -0300

    refactor(nvim/indent_blankline): move ibl config

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -636,29 +636,8 @@ return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		event = "LazyFile",
-		opts = {
-			indent = {
-				char = "│",
-				tab_char = "│",
-			},
-			scope = { show_start = false, show_end = false },
-			exclude = {
-				filetypes = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-				},
-			},
-		},
 		main = "ibl",
+		config = load_config("ibl"),
 	},
 	--: }}}
 }
