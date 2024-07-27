f38c34b4808fa619e50a4f18d04f3b0c95b35938
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-07-27 01:44:06 -0300

    refactor(nvim/neopywal): remove custom highlights

--- .config/nvim/lua/config/neopywal.lua
+++ .config/nvim/lua/config/neopywal.lua
@@ -33,38 +33,6 @@ neopywal.setup({
             tabline = true,
         },
     },
-    custom_highlights = function(C)
-        local U = require("neopywal.utils.color")
-        return {
-            CmpItemKindKeyword = { fg = C.keyword, styles = { "reverse" } },
-            CmpItemKindOperator = { fg = C.operator, styles = { "reverse" } },
-            CmpItemKindEnum = { link = "CmpItemKindKeyword" },
-            CmpItemKindFunction = { fg = C.func, styles = { "reverse" } },
-            CmpItemKindField = { fg = C.variable, styles = { "reverse" } },
-            CmpItemKindProperty = { fg = C.func, styles = { "reverse" } },
-            CmpItemKindMethod = { link = "CmpItemKindFunction" },
-            CmpItemKindColor = { fg = C.color3, styles = { "reverse" } },
-            CmpItemKindInterface = { fg = C.color4, styles = { "reverse" } },
-            CmpItemKindFolder = { fg = C.directory, styles = { "reverse" } },
-            CmpItemKindVariable = { fg = C.variable, styles = { "reverse" } },
-            CmpItemKindEvent = { link = "CmpItemKindVariable" },
-            CmpItemKindConstructor = { fg = C.special, styles = { "reverse" } },
-            CmpItemKindModule = { fg = C.include, styles = { "reverse" } },
-            CmpItemKindUnit = { fg = C.number, styles = { "reverse" } },
-            CmpItemKindSnippet = { link = "CmpItemKindModule" },
-            CmpItemKindClass = { fg = C.storageclass, styles = { "reverse" } },
-            CmpItemKindStruct = { fg = C.structure, styles = { "reverse" } },
-            CmpItemKindCopilot = { fg = C.color6, styles = { "reverse" } },
-            CmpItemKindTabNine = { fg = C.color6, styles = { "reverse" } },
-            CmpItemKindTypeParameter = { fg = C.identifier, styles = { "reverse" } },
-            CmpItemKindConstant = { fg = C.constant, styles = { "reverse" } },
-            CmpItemKindValue = { link = "CmpItemKindConstant" },
-            CmpItemKindEnumMember = { link = "CmpItemKindConstant" },
-            CmpItemKindText = { bg = U.lighten(C.background, 30), fg = C.foreground },
-            CmpItemKindFile = { link = "CmpItemKindText" },
-            CmpItemKindReference = { link = "CmpItemKindText" },
-        }
-    end,
 })
 
 neopywal.load()
