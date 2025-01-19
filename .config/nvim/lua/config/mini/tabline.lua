30c016f31a3ef0f2fcb7873c2e94f36f6cf9e591
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2025-01-19 16:52:51 -0300

    fix(nvim/mini.tabline): disable tabline on dashboard plugins

--- .config/nvim/lua/config/mini/tabline.lua
+++ .config/nvim/lua/config/mini/tabline.lua
@@ -4,6 +4,15 @@ if not has_tabline then return end
 local show_icons = true
 if vim.env.DISPLAY == nil then show_icons = false end
 
+-- Disable on certain filetypes
+local disabled_filetypes = { "snacks_dashboard", "dashboard", "alpha", "starter" }
+vim.api.nvim_create_autocmd("FileType", {
+    desc = "Auto disable mini.tabline on certain filetypes.",
+    group = vim.api.nvim_create_augroup("augroup_" .. "auto_disable_mini_tabline", { clear = true }),
+    pattern = disabled_filetypes,
+    callback = function() vim.b.minitabline_disable = true end,
+})
+
 tabline.setup({
     show_icons = show_icons,
     set_vim_settings = true, -- Whether to set Vim's settings for tabline (make it always shown and allow hidden buffers).
