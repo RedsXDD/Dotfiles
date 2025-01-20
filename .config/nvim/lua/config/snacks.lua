d8038e246787d89e9d55055cb1bcc7db696d0544
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2025-01-20 10:26:19 -0300

    refactor(nvim/snacks): disable input module

--- .config/nvim/lua/config/snacks.lua
+++ .config/nvim/lua/config/snacks.lua
@@ -59,41 +59,6 @@ snacks.setup({
         },
     },
     indent = { enabled = true },
-    input = {
-        enabled = true,
-        backdrop = true,
-        position = "float",
-        border = "rounded",
-        title_pos = "center",
-        height = 1,
-        width = 60,
-        relative = "editor",
-        noautocmd = true,
-        row = 2,
-        -- relative = "cursor",
-        -- row = -3,
-        -- col = 0,
-        wo = {
-            winhighlight = "NormalFloat:SnacksInputNormal,FloatBorder:SnacksInputBorder,FloatTitle:SnacksInputTitle",
-            cursorline = false,
-        },
-        bo = {
-            filetype = "snacks_input",
-            buftype = "prompt",
-        },
-        --- buffer local variables
-        b = {
-            completion = false, -- disable blink completions in input
-        },
-        keys = {
-            n_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "n", expr = true },
-            i_esc = { "<esc>", { "cmp_close", "stopinsert" }, mode = "i", expr = true },
-            i_cr = { "<cr>", { "cmp_accept", "confirm" }, mode = "i", expr = true },
-            i_tab = { "<tab>", { "cmp_select_next", "cmp" }, mode = "i", expr = true },
-            i_ctrl_w = { "<c-w>", "<c-s-w>", mode = "i", expr = true },
-            q = "cancel",
-        },
-    },
     lazygit = { enabled = true },
     notifier = {
         enabled = true,
