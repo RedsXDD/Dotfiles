bc04123d2307bc1cdce0a87e409268cc6cc421c5
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2025-01-21 11:11:55 -0300

    refactor(nvim/blink): move config options
    Moved the config options for blink.cmp to the plugins configuration
    directory

---
+++ .config/nvim/lua/reds/config/blink.lua
@@ -0,0 +1,59 @@
+local has_blink, blink = pcall(require, "blink.cmp")
+if not has_blink then return end
+
+blink.setup({
+    keymap = {
+        preset = "none",
+        ["<C-n>"] = { "show", "snippet_forward", "select_next" },
+        ["<C-p>"] = { "show", "snippet_backward", "select_prev" },
+
+        ["<C-j>"] = { "show", "select_next", "fallback" },
+        ["<C-k>"] = { "show", "select_prev", "fallback" },
+        ["<Tab>"] = { "show", "select_next", "fallback" },
+        ["<S-Tab>"] = { "show", "select_prev", "fallback" },
+
+        ["<C-space>"] = { "show", "show_documentation", "fallback" },
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+        ["<C-e>"] = { "hide", "fallback" },
+        ["<C-y>"] = { "select_and_accept", "fallback" },
+    },
+    appearance = {
+        use_nvim_cmp_as_default = false,
+        nerd_font_variant = "mono",
+        kind_icons = require("reds.icons").kinds,
+    },
+    sources = {
+        default = { "lsp", "snippets", "path", "buffer" },
+    },
+    signature = {
+        enabled = true,
+    },
+    snippets = { preset = "mini_snippets" },
+    completion = {
+        trigger = { prefetch_on_insert = true },
+        accept = {
+            auto_brackets = {
+                enabled = true,
+            },
+        },
+        menu = {
+            max_height = 15,
+            auto_show = true,
+            draw = {
+                padding = 0,
+                treesitter = { "lsp" },
+                columns = { { "kind_icon" }, { "label", "label_description" } },
+            },
+        },
+        documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 50,
+            update_delay_ms = 200,
+        },
+        ghost_text = {
+            enabled = true,
+        },
+    },
+})
--- .config/nvim/lua/reds/plugins.lua
+++ .config/nvim/lua/reds/plugins.lua
@@ -364,63 +364,8 @@ local misc = {
         dependencies = { "echasnovski/mini.snippets", "rafamadriz/friendly-snippets" },
         event = "InsertEnter",
         build = "cargo build --release",
-        opts = {
-            keymap = {
-                preset = "none",
-                ["<C-n>"] = { "show", "snippet_forward", "select_next" },
-                ["<C-p>"] = { "show", "snippet_backward", "select_prev" },
-
-                ["<C-j>"] = { "show", "select_next", "fallback" },
-                ["<C-k>"] = { "show", "select_prev", "fallback" },
-                ["<Tab>"] = { "show", "select_next", "fallback" },
-                ["<S-Tab>"] = { "show", "select_prev", "fallback" },
-
-                ["<C-space>"] = { "show", "show_documentation", "fallback" },
-                ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-                ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-                ["<C-e>"] = { "hide", "fallback" },
-                ["<C-y>"] = { "select_and_accept", "fallback" },
-            },
-            appearance = {
-                use_nvim_cmp_as_default = false,
-                nerd_font_variant = "mono",
-                kind_icons = require("reds.icons").kinds,
-            },
-            sources = {
-                default = { "lsp", "snippets", "path", "buffer" },
-            },
-            signature = {
-                enabled = true,
-            },
-            snippets = { preset = "mini_snippets" },
-            completion = {
-                trigger = { prefetch_on_insert = true },
-                accept = {
-                    auto_brackets = {
-                        enabled = true,
-                    },
-                },
-                menu = {
-                    max_height = 15,
-                    auto_show = true,
-                    draw = {
-                        padding = 0,
-                        treesitter = { "lsp" },
-                        columns = { { "kind_icon" }, { "label", "label_description" } },
-                    },
-                },
-                documentation = {
-                    auto_show = true,
-                    auto_show_delay_ms = 50,
-                    update_delay_ms = 200,
-                },
-                ghost_text = {
-                    enabled = true,
-                },
-            },
-        },
-        opts_extend = { "sources.default" },
+        config = load_config("blink"),
+        -- opts_extend = { "sources.default" },
     },
     --: }}}
     --: hardtime.nvim {{{
